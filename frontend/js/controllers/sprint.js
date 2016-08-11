80dcff6df36c2f1ab7045098de0db0e0229f6391
Iain Duff <iain_duff@scee.net>
2016-08-11 15:57:20 +0000

    We now have audio

--- frontend/js/controllers/sprint.js
+++ frontend/js/controllers/sprint.js
@@ -7,18 +7,36 @@ const controller = function controller ($scope, $sce, backendService) {
   const projectId = "19220804858";
   const updateInterval = 1000 * 5;
   const displayLength = 1000 * 30;
-  const displayFlashInterval = 100;
   const scheduleStateWeCareAbout = "Accepted";
 
   let hasUpdatedIterationYet = false;
 
+
   $scope.iterations = [];
   $scope.currentIteration = null;
   $scope.currentIterationTickets = [];
+  $scope.currentMp3Url = null;
+
+  const estimateMp3StartTimesSeconds = {
+    "1": {
+      "start": 24
+    },
+    "2": {
+      "start": 0
+    },
+    "3": {
+      "start": 30
+    },
+    "5": {
+      "start": 20
+    },
+    "8": {
+      "start": 4
+    }
+  };
 
   $scope.manuallyUpdateIteration = function manuallyUpdateIteration () {
     hasUpdatedIterationYet = false;
-    $scope.updateIteration();
   };
 
   $scope.updateIteration = function updateIteration () {
@@ -51,33 +69,29 @@ const controller = function controller ($scope, $sce, backendService) {
 
       let estimate = ticketsThatAreNowAcceptedAndWerentBefore[0].PlanEstimate || 1;
 
+
       $scope.display(estimate);
 
     });
   };
 
   $scope.display = function display (estimate) {
+    $scope.currentMp3Url = `audio/${estimate}.mp3`;
+    document.getElementById("woof").onplay = function() {
+      this.currentTime = estimateMp3StartTimesSeconds[estimate.toString()].start;
+      console.log(`this.currentTime = ${this.currentTime}`);
+    }
+
     let el = document.getElementById("display");
     el.style.backgroundImage = `url('img/${estimate}.gif')`;
     el.style.display = "block";
 
-    var makeComponent = () => {
-      return Math.floor(Math.random() * 255).toString();
-    };
-
-    let displayFlashIntervalReference = setInterval(() => {
-      document.body.style.backgroundColor = `rgb(${makeComponent()}, ${makeComponent()}, ${makeComponent()})`;
-    }, displayFlashInterval);
-
     setTimeout(() => {
       el.style.backgroundImage = "";
       el.style.display = "none";
-      document.body.style.backgroundColor = "";
-      clearInterval(displayFlashIntervalReference);
+      $scope.currentMp3Url = null;
     }, displayLength);
   };
-
-  $scope.display(1);
 
   backendService.getIterations(projectId, (err, iterations) => {
     if (err) return;
